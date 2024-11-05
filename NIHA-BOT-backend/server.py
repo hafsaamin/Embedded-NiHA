a620d5db38029bee35663ba4cbce8316eda75a38
099-webtech <sp21-bcs-099@cuilahore.edu.pk>
2024-11-05 22:08:03 +0500

    Initial commit

--- NIHA-BOT-backend/server.py
+++ NIHA-BOT-backend/server.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 CORS(app, origins=['http://localhost:3000'])  # Adjust the origin as needed
 
 # Set your OpenAI API key
-ok
+
 openai.api_key = OPENAI_API_KEY  # Set the API key for OpenAI
 
 # MongoDB connection
